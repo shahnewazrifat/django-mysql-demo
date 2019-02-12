5ab0905c13a1159007c28814295577f94c469c16
Robert Aldrich <robert.aldrich@devfactory.com>
2019-02-12 08:19:03 -0500

    Cleaned up code for demo. Switched file to modify to a py file.

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Hello, from DevSpaces!</h2> <br><a href='/admin'>Login</a>")
+    return HttpResponse("<h2 align='center'>Your app and DevSpace are running in the CLOWD!</h2> <div align='center'><a href='/admin'>Login</a></div>")
